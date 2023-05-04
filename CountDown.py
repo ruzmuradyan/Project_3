9abff93c29531fbfcb60631756bf1171ad2af44b
Ruzanna <90098750+ruzmuradyan@users.noreply.github.com>
2023-05-04 12:14:54 +0400

    Add files via upload

---
+++ CountDown.py
@@ -0,0 +1,16 @@
+import time
+theTime = input("Insert time to count down (HH:MM:SS)") #The user is prompted to input a time in the format of "HH:MM:SS"
+
+
+b=[int(i) for i in theTime.split(":")]  #The theTime.split(":") function splits the string into three separate strings, one for each component of the time (hours, minutes, and seconds), using the colon as a delimiter.The resulting list of strings is then passed into a list comprehension that converts each string into an integer using the int() function. This creates a list of three integers representing the hours, minutes, and seconds inputted by the user.
+second=b[0]*3600+b[1]*60+b[2]  #the number of seconds is calculated by multiplying the hours by 3600 (the number of seconds in an hour), adding the minutes multiplied by 60 (the number of seconds in a minute), and adding the remaining seconds. This calculation returns the total number of seconds for the countdown timer.
+
+
+for x in range(second, 0, -1):  #The range function is used with three arguments: second is the total number of seconds for the countdown, 0 is the stopping point of the countdown (when it reaches zero), and -1 is the step value, which counts down by one at each iteration of the loop.
+     sec = x % 60           #the number of seconds remaining (x) is converted back into hours, minutes, and seconds using integer division and modulo operations.
+     minute = int(x / 60) % 60
+     hours = int(x / 3600)
+     print(f'{hours:02}:{minute:02}:{sec:02}')
+     time.sleep(1 / 2)  #The time.sleep() function is used to pause the program for half a second before the next iteration of the loop, which creates the appearance of a ticking countdown clock.
+time.sleep(2)
+print("Done")
